890d807b4834b5a008e711b85ceaeac4cc838999
Unknown <michael.santi@web.de>
2018-06-15 14:35:29 +0200

    gitIgnore changes

--- out/gcc-debug/src/routing/QoSRequirementsMapping.d
+++
@@ -1,5 +0,0 @@
-out/gcc-debug//src/routing/QoSRequirementsMapping.o: \
- src/routing/QoSRequirementsMapping.cc \
- src/routing/QoSRequirementsMapping.h
-
-src/routing/QoSRequirementsMapping.h:
--- out/gcc-release/src/routing/QoSRequirementsMapping.d
+++
@@ -1,5 +0,0 @@
-out/gcc-release//src/routing/QoSRequirementsMapping.o: \
- src/routing/QoSRequirementsMapping.cc \
- src/routing/QoSRequirementsMapping.h
-
-src/routing/QoSRequirementsMapping.h:
